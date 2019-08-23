359b9b7218434e2664698c0d9d408f5032e93520
fokayx <fokayx@gmail.com>
2019-08-23 15:52:29 +0800

    Fixed type and adjust coding style

--- src/App.js
+++ src/App.js
@@ -8,80 +8,79 @@ import './index.css';
 
 const Messages = props => props.data.map(m => m[0] !== '' ? (<li><strong>{m[0]}</strong> : <div className="innermsg">{m[1]}</div></li>) : (<li className="update">{m[1]}</li>) );
 
-const Online = props => props.data.map(m => <li id={m[0]}>{m[1]}</li>)
+const Online = props => props.data.map(m => <li id={m[0]}>{m[1]}</li>);
 
 export default () => {
   const [id, setId] = useState('');
-  const [nameInput, setNameInput] = useState('');
+  const [name, setNameInput] = useState('');
   const [room, setRoom] = useState('');
   const [input, setInput] = useState('');
 
   const [socket] = useSocket('https://open-chat-naostsaecf.now.sh');
-	socket.connect();
+  socket.connect();
 
   const [messages, setMessages] = useImmer([]);
-
   const [online, setOnline] = useImmer([]);
 
-  useEffect(()=>{
-    socket.on('message que',(nick,message) => {
+  useEffect(() => {
+    socket.on('message que', (nick, message) => {
       setMessages(draft => {
-        draft.push([nick,message])
+        draft.push([nick, message])
       })
     });
 
-    socket.on('update',message => setMessages(draft => {
-      draft.push(['',message]);
-    }))
+    socket.on('update', message => setMessages(draft => {
+      draft.push(['', message]);
+    }));
 
-    socket.on('people-list',people => {
+    socket.on('people-list', people => {
       let newState = [];
-      for(let person in people){
-        newState.push([people[person].id,people[person].nick]);
+      for (let person in people) {
+        newState.push([people[person].id, people[person].nick]);
       }
       setOnline(draft=>{draft.push(...newState)});
       console.log(online)
     });
 
-    socket.on('add-person',(nick,id)=>{
+    socket.on('add-person', (nick, id) => {
       setOnline(draft => {
-        draft.push([id,nick])
+        draft.push([id, nick])
       })
-    })
+    });
 
-    socket.on('remove-person',id=>{
+    socket.on('remove-person', id => {
       setOnline(draft => draft.filter(m => m[0] !== id))
-    })
+    });
 
-    socket.on('chat message',(nick,message)=>{
+    socket.on('chat message', (nick, message) => {
       setMessages(draft => {draft.push([nick,message])})
-    })
-  },0);
+    });
+  }, 0);
 
   const handleSubmit = e => {
     e.preventDefault();
-    if (!nameInput) {
+    if (!name) {
       return alert("Name can't be empty");
     }
-    setId(nameInput);
-    socket.emit("join", nameInput,room);
+    setId(name);
+    socket.emit("join", name, room);
   };
 
   const handleSend = e => {
     e.preventDefault();
     if(input !== ''){
-      socket.emit('chat message',input,room);
+      socket.emit('chat message', name, room);
       setInput('');
     }
-  }
+  };
 
   return id ? (
-    <section style={{display:'flex',flexDirection:'row'}} >
+    <section style={{display:'flex', flexDirection:'row'}} >
       <ul id="messages"><Messages data={messages} /></ul>
-      <ul id="online"> 🌐 : <Online data={online} /> </ul>
+      <ul id="online"> &#x1f310; : <Online data={online} /> </ul>
       <div id="sendform">
         <form onSubmit={e => handleSend(e)} style={{display: 'flex'}}>
-            <input id="m" onChange={e=>setInput(e.target.value.trim())} /><button style={{width:'75px'}} type="submit">Send</button>
+            <input id="msg" value={input} onChange={e => setInput(e.target.value.trim())} /><button style={{width:'75px'}} type="submit">Send</button>
         </form>
       </div>
     </section>
